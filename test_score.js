81048cc7267152254895c5d9ae352172b403f0d5
Varnikumar Patel <varnikumar1512@gmail.com>
2026-04-17 05:35:04 +0530

    chore: remove obsolete test and debugging scripts

--- test_score.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const { PDFParse } = require('pdf-parse');
-const { calculateATSScore } = require('./src/lib/atsScoring.ts'); // Need ts-node or just rewrite to js
-
